08e8acbea3a6bc566ce39e178b44bdd467e7a055
Xeroas <145765807+Xeroas@users.noreply.github.com>
2024-04-22 23:21:13 +0100

    Update Die.cs
    Work as of 22.04.24

--- CMP1903_Assessment2/Die.cs
+++ CMP1903_Assessment2/Die.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,18 +8,7 @@ namespace CMP1903_A2
 {
     internal class Die
     {
-        /*
-         * The Die class should contain one property to hold the current die value,
-         * and one method that rolls the die, returns and integer and takes no parameters.
-         */
 
-        /// <summary>
-        /// The "Die" class is responsible for defining the Die and assigning it a random value within the range 1-6. 
-        /// this is done with the help of the "Roll" method which generates the die values within the expected range.
-        /// </summary>
-
-
-        //Property
         private int _dieCurVal; //Private variable created to hold the current value of the die privately (Encapsulated).
 
         public int pubCurVal
@@ -45,15 +34,20 @@ namespace CMP1903_A2
 
             if (sum == 7)
             {
-                Console.WriteLine("game over / player swap");
+                //Console.WriteLine("End of turn!");
                 _playerSwap = true; // When true players swap
             }
+            else
+            {
+                _playerSwap = false; //Set to false if sum is not 7 (stops false swapping after first 7 is rolled)
+            }    
 
             return (sum, roll1, roll2); //Returning the sum of the dies, and the individual die values.
         }
-        public bool SwapPlayers()
+
+        public void SwapPlayers()
         {
-            return _playerSwap;
+            _playerSwap = false;
         }
     }
 }
