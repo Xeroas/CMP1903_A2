598f0a6f6bb126879eccb811f38ab5e4d57c4eb8
Xeroas <145765807+Xeroas@users.noreply.github.com>
2024-05-02 01:34:33 +0100

    Update Testing.cs
    Final Ver

--- CMP1903_Assessment2/Testing.cs
+++ CMP1903_Assessment2/Testing.cs
@@ -15,13 +15,21 @@ namespace CMP1903_A2
             Statistics testGame = new Statistics(); //Object instantiation, loading "Game" into the testing class.
             Die testDie = new Die();
 
-            for (int i = 0; i < 200; i++)  //For loop to repeatedly test the dies, in this case they are tested 1000 times.
+            for (int i = 0; i < 200; i++)  //For loop to repeatedly test the dice, in this case they are tested 200 times (Rarely runs all 200 though as it will stop once a sum of 7 is detected).
             {
-                var testGameResult = testDie.StartGameSevens();  //Starts a game named "testGameResult" within the loop (1000 times) in order to test die values.
+                var testGameResult = testDie.StartGameSevens();  //Starts a game named "testGameResult" within the loop (200 times) in order to test die values.
 
-                Debug.Assert(testGameResult.Item1 >= 2 && testGameResult.Item1 <= 12, "Sum Value is out of expected range"); //Checks the sum of the three dies is no less than 3 and no more than 18.
-                Debug.Assert(testGameResult.Item2 >= 1 && testGameResult.Item2 <= 6, "Die Value is out of expected range"); //Checks that the value of Die 1 is no less than 1 and no more than 6
+                Debug.Assert(testGameResult.Item1 >= 2 && testGameResult.Item1 <= 12, "Sum Value is out of expected range");// Check if the sum of the dice is within the expected range (2 to 12)
+                Debug.Assert(testGameResult.Item2 >= 1 && testGameResult.Item2 <= 6, "Die Value is out of expected range");// Check if each die value is within the expected range (1 to 6)
                 Debug.Assert(testGameResult.Item3 >= 1 && testGameResult.Item3 <= 6, "Die Value is out of expected range");
+
+                // Check if the game stops when the sum of the dice is 7
+                if (testGameResult.Item1 == 7)
+                {
+                    Debug.Assert(testGameResult.Item2 + testGameResult.Item3 == 7, "Game didn't stop when sum is 7");
+                    Console.WriteLine("Die sum of 7 detected! Stopping game!");
+                    break; // Stop testing once sum of 7 detected
+                }
             }
         }
     }
